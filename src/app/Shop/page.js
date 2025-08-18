f813c14a1619da1225a17dfc071cc46280a160b1
[Pradeep-incrix] <[pradeepsurya@incrix.in]>
2025-08-18 23:00:16 +0530

    update

---
+++ src/app/Shop/page.js
@@ -0,0 +1,12 @@
+import Stack from "@mui/material/Stack";
+import NavBar from "@/src/app/Components/HomePage/navBar";
+import Footer from "@/src/app/Components/HomePage/footer";
+
+export default function Shop() {
+  return (
+    <Stack>
+      <NavBar />
+      <Footer />
+    </Stack>
+  );
+}
